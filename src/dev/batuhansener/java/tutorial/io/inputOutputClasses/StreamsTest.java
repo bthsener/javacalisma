bb8071e4df4037228be38f2175b5bf370c6257cc
abatuhan06 <abatuhan0666@gmail.com>
2023-04-24 14:56:10 +0300

    example of byte to string added

--- src/dev/batuhansener/java/tutorial/io/inputOutputClasses/StreamsTest.java
+++ src/dev/batuhansener/java/tutorial/io/inputOutputClasses/StreamsTest.java
@@ -6,13 +6,18 @@ public class StreamsTest {
     public static void main(String[] args) {
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\batuhan\\Desktop\\StreamsTest\\read.txt"));
-            String i = reader.readLine();
-            System.out.println(i);
-            reader.close();
-            FileWriter fileWriter = new FileWriter("C:\\Users\\batuhan\\Desktop\\StreamsTest\\read.txt");
-            fileWriter.append("denemedeneme");
-            fileWriter.close();
+//            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\batuhan\\Desktop\\StreamsTest\\read.txt"));
+//            String i = reader.readLine();
+//            System.out.println(i);
+//            reader.close();
+//            FileWriter fileWriter = new FileWriter("C:\\Users\\batuhan\\Desktop\\StreamsTest\\read.txt");
+//            fileWriter.append("denemedeneme");
+//            fileWriter.close();
+            InputStream inputStream = new FileInputStream("C:\\Users\\batuhan\\Desktop\\StreamsTest\\read.txt");
+            byte[] bytes = new byte[inputStream.available()];
+            inputStream.read(bytes);
+            String responseBody = new String(bytes);
+            System.out.println(responseBody);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }catch (IOException e){
